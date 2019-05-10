a6508ff82ef0d87ae9f0c446cc6ba9a77ced930a
Shaikh farhan <31347917+farhanpro@users.noreply.github.com>
2019-05-10 21:09:22 +0530

    Add files via upload

---
+++ math.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+	"fmt"
+	"math")
+	
+func main(){
+	fmt.Println("Square root of 49 is")
+	fmt.Println(math.Sqrt(49))
+}
---
+++ math.min.go
@@ -0,0 +1,12 @@
+package main
+
+import(
+"fmt"
+"math"
+)
+func main(){
+	 a , b := 33.5, 33.4
+	  c:= math.Min(a,b)
+	fmt.Println("Smallest integer is",c)
+	
+	}
