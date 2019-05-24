b13d58d87bdf9de34ca9ae75694658b6b12bcced
Shaikh farhan <31347917+farhanpro@users.noreply.github.com>
2019-05-25 01:58:57 +0530

    Add files via upload

---
+++ api.go
@@ -0,0 +1,34 @@
+package main
+import (
+	"fmt"
+	"log"
+	"net/http"
+//	"encoding/json"
+)
+/*
+type Article struct {
+	Title string 'json:"Title"'
+	Desc string 'json:"desc"'
+	Content string 'json:"content"'
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+	Article{Title:"Test Title",Desc:"Test Description", Content : "Hello World"},
+}
+*/
+func homePage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,"Homepage Endpoint Hit")
+}
+
+func handleRequests(){
+	http.HandleFunc("/",homePage)
+	//http.HandleFunc("/articles",allArticles)
+	log.Fatal(http.ListenAndServe(":8081",nil))
+}
+
+func main(){
+	handleRequests()
+}
